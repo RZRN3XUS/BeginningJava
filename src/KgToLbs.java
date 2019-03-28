ea24366e7c369168edd50f2705f52ec9d044f4d0
Christopher Dycus <rzrn3xus@gmail.com>
2019-03-28 15:18:18 -0500

    commit successful

--- src/KgToLbs.java
+++ src/KgToLbs.java
@@ -1,2 +1,16 @@
 public class KgToLbs {
-}
+        public static void main(String[] args) {
+            double convrate = 0.453592;
+            double kg = 10;
+            double kg2 = 50;
+            double kg3 = 100;
+
+            double lbs;
+            System.out.println("10 KG = " +kg*convrate +"lbs");
+            System.out.println("50 KG = " +kg2*convrate +"lbs");
+            System.out.println("100 KG = " +kg3*convrate +"lbs");
+
+        }
+    }
+
+
