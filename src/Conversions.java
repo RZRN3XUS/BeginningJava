34f5f3f974cd61e2305c58d508c4dc1a3b2f45bc
Christopher Dycus <rzrn3xus@gmail.com>
2019-04-17 09:35:28 -0500

    ;)

--- src/Conversions.java
+++ src/Conversions.java
@@ -2,16 +2,20 @@ import java.util.*;
 public class Conversions {
     public static void main(String[] args) {
         String input;
+        String hex1 = "Hex";
+        String hex2 = "hex";
+        String hex3 = "Hexadecimal";
+        String hex4 = "hexadecimal";
         Scanner read = new Scanner(System.in);
         System.out.println("Convert between Hex, Binary, and Decimal. Please enter unit that is being converted from.");
         input = read.nextLine();
-        if (input == "Hex") {
+        if (input.equals(hex1)) {
             hexpre(input);
-        } else if (input == "hex") {
+        } else if (input.equals(hex2) ) {
             hexpre(input);
-        } else if (input == "Hexadecimal") {
+        } else if (input.equals(hex4)) {
             hexpre(input);
-        } else if (input == "hexadecimal"){
+        } else if (input.equals(hex3)){
             hexpre(input);
     }
 
@@ -20,42 +24,49 @@ public class Conversions {
     public static void hexpre(String input){
         Scanner sc = new Scanner(System.in);
         int value;
+        String input2;
+        String hex1 = "Hex";
+        String hex2 = "hex";
+        String hex3 = "Hexadecimal";
+        String hex4 = "hexadecimal";
+        String dec1 = "decimal";
+        String dec2 = "Decimal";
+        String bin1 = "binary";
+        String bin2 = "Binary";
         System.out.println("What are you converting to?");
-        if (input == "Hex") {
+        input2 = sc.nextLine();
+        if (input2.equals(hex1)) {
             System.out.println("Invalid input.");
-        } else if (input == "hex") {
+        } else if (input2.equals(hex2)) {
             System.out.println("Invalid input.");
-        } else if (input == "Hexadecimal") {
+        } else if (input2.equals(hex3)) {
             System.out.println("Invalid input.");
-        } else if (input == "hexadecimal") {
+        } else if (input2.equals(hex4)) {
             System.out.println("Invalid input.");
         }
-        else if (input == "Binary"){
+        else if (input2.equals(bin1)){
             System.out.println("What is the number to be converted?");
             value = sc.nextInt();
             hextobin(value);
         }
-        else if (input == "binary"){
+        else if (input2.equals(bin2)){
             System.out.println("What is the number to be converted?");
             value = sc.nextInt();
             hextobin(value);
         }
-        else if (input == "Decimal"){
+        else if (input2.equals(dec1)){
             System.out.println("What is the number to be converted?");
             value = sc.nextInt();
             hextodec(value);
         }
-        else if (input == "decimal"){
+        else if (input2.equals(dec2)){
             System.out.println("What is the number to be converted?");
             value = sc.nextInt();
             hextodec(value);
-        }
-        else {
-            System.out.println("Invalid Input");
         }
     }
     public static void hextodec(int value){
-        System.out.println("poscheck");
+
     }
     public static void hextobin(int value){
 
